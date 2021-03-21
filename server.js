c8fdd1eec8da16f889afd73f8939b64fd324cd17
chidoukaigwe <chidodesigns@gmail.com>
2021-03-21 23:47:46 +0000

    modified server.js file for heroku build/deployment

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const cors = require('cors')
 const connectDB = require("./config/db")
     //  Server Port
 const PORT = process.env.PORT || 5000
+
+    //  Path
+const path = require('path')
+
     //  Express App
 const app = express()
     //  DB Connection
 connectDB()
     //  Init Middleware
 app.use(express.json({ extended: false }))
-
-app.get("/", (req, res) => {
-    res.send("API Running")
-})
 
 app.use(cors())
 
@@ -24,6 +24,15 @@ app.use("/api/users", require("./routes/api/users"))
 app.use("/api/auth", require("./routes/api/auth"))
 app.use("/api/profile", require("./routes/api/profile"))
 app.use("/api/posts", require("./routes/api/posts"))
+
+//  Serve Static Assets In Prod
+if(process.env.NODE_ENV === 'production') {
+    //  Set Static Folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client',  'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server Started On Port ${PORT}`)
